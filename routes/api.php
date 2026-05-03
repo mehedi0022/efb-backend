6de66cb4cc285e5ae75ebb8bc3b32012e0e1572b
mehedi0022 <mdmehedihassan0022@gmail.com>
2026-05-03 15:51:08 +0600

    Facebook Conversion Api

--- routes/api.php
+++ routes/api.php
@@ -39,8 +39,14 @@ use App\Http\Controllers\Api\SettingController;
 use App\Http\Controllers\Api\ShippingChargeController;
 use App\Http\Controllers\Api\SiteDataController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\FacebookTrackingController;
 
 Route::prefix('v1')->middleware('ip.block')->group(function () {
+    // Facebook CAPI endpoint
+    Route::post('/v1/facebook/track-purchase', 
+    [FacebookTrackingController::class, 'trackPurchase']
+    );
+
     // Public content endpoints
     Route::get('/settings', [SettingController::class, 'show']);
     Route::get('/site-data', [SiteDataController::class, 'index']);
